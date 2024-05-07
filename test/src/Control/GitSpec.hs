5b043fd1c747e1a86ae93be5873f7cacbc987fe0
Christian Gram Kalhauge <chrg@dtu.dk>
2024-05-07 12:30:15 +0200

    Remove some unessary dependecies

--- test/src/Control/GitSpec.hs
+++ test/src/Control/GitSpec.hs
@@ -5,34 +5,34 @@ module Control.GitSpec (test) where
 import Control.Git (gitChanges)
 import System.Directory (createDirectoryIfMissing, removeDirectoryRecursive)
 import System.Directory.Internal.Prelude (tryIOError)
+import Test.Hspec.Expectations.Pretty
 import Test.Tasty
-import Test.Tasty.HUnit
+import Test.Tasty.Expected
 
 test :: TestTree
 test =
   testGroup
     "Control.Git"
-    [ testCase "find unadded folder" do
+    [ it "find unadded folder" do
         _ <- tryIOError (removeDirectoryRecursive "test/git/unadded")
         createDirectoryIfMissing True "test/git/unadded"
         writeFile "test/git/unadded/test1.txt" "test1"
         writeFile "test/git/unadded/test2.txt" "test2"
-        changes <- gitChanges ["test/git/unadded"]
-        changes
-          @?= [ "test/git/unadded/test1.txt"
-              , "test/git/unadded/test2.txt"
-              ]
+        gitChanges ["test/git/unadded"]
+          `shouldReturn` [ "test/git/unadded/test1.txt"
+                         , "test/git/unadded/test2.txt"
+                         ]
         removeDirectoryRecursive "test/git/unadded"
-    , testCase "changes in added file" do
+    , it "changes in added file" do
         createDirectoryIfMissing True "test/git/"
 
         writeFile "test/git/added.txt" "same as always"
-        changes <- gitChanges ["test/git/added.txt"]
-        changes @?= []
+        gitChanges ["test/git/added.txt"]
+          `shouldReturn` []
 
         writeFile "test/git/added.txt" "a change"
-        changes2 <- gitChanges ["test/git/added.txt"]
-        changes2 @?= ["test/git/added.txt"]
+        gitChanges ["test/git/added.txt"]
+          `shouldReturn` ["test/git/added.txt"]
 
         writeFile "test/git/added.txt" "same as always"
     ]
